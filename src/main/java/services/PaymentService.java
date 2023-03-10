531f1ef983c3c670f9d7438c3a49f722172f8cbd
Phạm Văn Nguyên <npv2k1@gmail.com>
2023-03-10 11:49:18 +0700

    Initial commit

---
+++ src/main/java/services/PaymentService.java
@@ -0,0 +1,14 @@
+package services;
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.jws.soap.SOAPBinding.Style;
+
+@WebService
+@SOAPBinding(style = Style.RPC)
+public class PaymentService {
+    @WebMethod
+    public String sayHello(String name) {
+        return "Hello " + name + "!";
+    }
+}
